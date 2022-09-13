ea20001c193c5155f69a766cfea46ce732d390f0
Johnbosco <nwekejohnbosco@gmail.com>
2022-09-13 11:36:52 +0100

    add husky, eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'no-console': 2,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase'],
+      },
+    ],
+  },
+};
